1aa0c54a556e0285bdef6957688eb94ad180702f
Beth Sutton <bksutton2@gmail.com>
2022-04-16 14:08:32 -0600

    submit a note

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -6,7 +6,11 @@ import { toast } from 'react-toastify';
 import Modal from 'react-modal';
 import { FaPlus } from 'react-icons/fa';
 import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
-import { getNotes, reset as notesReset } from '../features/notes/noteSlice';
+import {
+  getNotes,
+  createNote,
+  reset as notesReset,
+} from '../features/notes/noteSlice';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import NoteItem from '../components/NoteItem';
@@ -66,7 +70,7 @@ function Ticket() {
   const onNoteSubmit = (e) => {
     e.preventDefault();
 
-    console.log('Submit');
+    dispatch(createNote({ noteText, ticketId }));
     closeModal();
   };
 
